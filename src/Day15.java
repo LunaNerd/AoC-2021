4dbd45d1e7b38e417030cf2f8799a65c5994d3b6
Luna Versmissen <luna.versmissen@ugent.be>
2021-12-15 19:43:07 +0100

    part 1 working, part 2 wip

---
+++ src/Day15.java
@@ -0,0 +1,147 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Day15 {
+    public static int[][] levelMap;
+
+    //contains the lowest sum of danger to get to a certain position
+    public static int[][] minSum;
+    public static int size;
+
+    public static void main(String[] args) throws FileNotFoundException {
+        readMapFromFile();
+        //convertToBigMap();
+
+        minSum = new int[size][size];
+        for (int y = 0; y<size; y++) {
+            for (int x = 0; x<size; x++) {
+                minSum[y][x] = Integer.MAX_VALUE;
+            }
+        }
+        minSum[0][1] = levelMap[0][1];
+        minSum[1][0] = levelMap[1][0];
+
+        long oldGrandSum = 0;
+        long grandSum = 2;
+
+        while (oldGrandSum != grandSum) {
+            oldGrandSum = grandSum;
+            grandSum = 0;
+            for (int y = 0; y<size; y++) {
+                for (int x = 0; x<size; x++) {
+                    int l = lowestSurroundingSum(y, x);
+                    if (l != Integer.MAX_VALUE && minSum[y][x] > l + levelMap[y][x]) {
+                        minSum[y][x] = l + levelMap[y][x];
+                    }
+                    grandSum += minSum[y][x];
+                }
+            }
+
+            for (int y = 0; y<size; y++) {
+                for (int x = 0; x<size; x++) {
+                    if (minSum[y][x] == Integer.MAX_VALUE) {
+                        System.out.print(".");
+                    }
+                    else {
+                        System.out.print(minSum[y][x] + "\t");
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+        System.out.println(minSum[size-1][size-1]);
+    }
+
+    public static int lowestSurroundingSum(int y, int x) {
+        int min = Integer.MAX_VALUE;
+        int vert = y-1;
+        int hor = x;
+        if (vert >= 0 && vert < size && hor >= 0 && hor < size) {
+            if (minSum[vert][hor] < min) {
+                min = minSum[vert][hor];
+            }
+        }
+        vert = y+1;
+        hor = x;
+        if (vert >= 0 && vert < size && hor >= 0 && hor < size) {
+            if (minSum[vert][hor] < min) {
+                min = minSum[vert][hor];
+            }
+        }
+        vert = y;
+        hor = x-1;
+        if (vert >= 0 && vert < size && hor >= 0 && hor < size) {
+            if (minSum[vert][hor] < min) {
+                min = minSum[vert][hor];
+            }
+        }
+        vert = y;
+        hor = x+1;
+        if (vert >= 0 && vert < size && hor >= 0 && hor < size) {
+            if (minSum[vert][hor] < min) {
+                min = minSum[vert][hor];
+            }
+        }
+        return min;
+    }
+
+    /*public static void convertToBigMap() {
+        int[][] bigMap = new int[size*5][size*5];
+        for (int y = 0; y<size; y++) {
+            for (int x = 0; x<size; x++) {
+                for (int ver = 0; ver < 5; ver++) {
+                    for (int hor = 0; hor < 5; hor++) {
+                        bigMap[ver*size + y][hor*size + x] = (levelMap[y][x] + ver + hor) % 10;
+                    }
+                }
+            }
+        }
+        levelMap = bigMap;
+        size *= 5;
+        for (int y = 0; y<size; y++) {
+            if (y%(size/5) == 0) {
+                System.out.print("\n");
+            }
+            for (int x = 0; x<size; x++) {
+                if (x%(size/5) == 0) {
+                    System.out.print(" ");
+                }
+                System.out.print(levelMap[y][x]);
+            }
+            System.out.println();
+        }
+    }*/
+
+    public static void readMapFromFile() throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("Day15_s.txt"));
+
+        String[] split = sc.nextLine().split("");
+        size = split.length;
+        levelMap = new int[size][size];
+        int x = 0;
+        int y = 0;
+        for (String s : split) {
+            levelMap[y][x] = Integer.parseInt(s);
+            x++;
+        }
+        y++;
+        x=0;
+        while (sc.hasNextLine()) {
+            split = sc.nextLine().split("");
+            x = 0;
+            for (String s : split) {
+                levelMap[y][x] = Integer.parseInt(s);
+                x++;
+            }
+            y++;
+        }
+        /*for (y = 0; y<size; y++) {
+            for (x = 0; x<size; x++) {
+                System.out.print(levelMap[y][x]);
+            }
+            System.out.println();
+        }*/
+    }
+}
